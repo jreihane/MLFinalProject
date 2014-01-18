5fc4031e0085651af69b68014d4d9a07f4bc4ecc
Reihane <reihane.zekri@gmail.com>
2014-01-18 17:38:26 +0330

    last

--- src/uni/ml/Main.java
+++ src/uni/ml/Main.java
@@ -22,165 +22,181 @@ public class Main {
 	 */
 	@SuppressWarnings("unused")
 	public static void main(String[] args) {
-		String fileName = "";
-		try {
-			BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
-			fileName = bufferRead.readLine();
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-			fileName = "iris";
-		}
+//		String fileName = "";
+//		try {
+//			BufferedReader bufferRead = new BufferedReader(new InputStreamReader(System.in));
+//			fileName = bufferRead.readLine();
+//		} catch (IOException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//			fileName = "iris";
+//		}
 		
-		BufferedReader dataFile = null;
-		try {
-			Instances inputTrain = null;
-			 
-			BasicMethods bestMethod = null;
-			// NaiveBayes nB = new NaiveBayes();
-
-			BasicDiscretizers basicDiscretizers = new BasicDiscretizers();
-			ClassifierOperations classifierOps = new ClassifierOperations();
-
-			dataFile = basicDiscretizers
-					.readDataFile("C:\\Program Files\\Weka-3-7\\data\\new\\" + fileName + ".arff");
-			inputTrain = basicDiscretizers.loadData(dataFile);
-
-			// 1- rank continuous attributes by the nonparametric measure.
-			Map<Attribute, Double> dependencyLevel = classifierOps
-					.getDepencencyLevel(inputTrain);
-			List<Attribute> sortedDependencies = classifierOps
-					.sortDependencyLevels(dependencyLevel);
-
-			// 2- for all continuous attributes,
-			// find the best one that achieves the largest prediction accuracy
-			// from the four basic discretization methods {
-
-			// 2-1 Equal-width discretization
-			double accuracy1 = applyDiscretization(BasicMethods.EQUAL_WIDTH,
-					inputTrain, null).getAccuracy();
-//			System.out.println(accuracy1);
-
-			// 2-2 Equal-frequency discretization
-//			System.out.println("=============================================");
-
-			double accuracy2 = applyDiscretization(
-					BasicMethods.EQUAL_FREQUENCY, inputTrain, null).getAccuracy();
-//			System.out.println(accuracy2);
-
-			// 2-3 Entropy-based discretization
-//			System.out.println("=============================================");
-			double accuracy3 = applyDiscretization(BasicMethods.ENTROPY_BASED,
-					inputTrain, null).getAccuracy();
-//			System.out.println(accuracy3);
-
-			// 2-4 find the highest accuracy among all accuracies
-			double max = Math.max(accuracy1, accuracy2);
-			max = Math.max(max, accuracy3);
-			if (max == accuracy1)
-				bestMethod = BasicMethods.EQUAL_WIDTH;
-			else if (max == accuracy2)
-				bestMethod = BasicMethods.EQUAL_FREQUENCY;
-			else if (max == accuracy3)
-				bestMethod = BasicMethods.ENTROPY_BASED;
-			
-			NaiveBayes nB1 = new NaiveBayes();
-			nB1.buildClassifier(inputTrain);
-			Evaluation eval2 = new Evaluation(inputTrain);
-			eval2.crossValidateModel(nB1, inputTrain, 5, new Random(1));
-			System.out.println("first is: " + classifierOps.calculateAccuracy(eval2.predictions()));
-
-			// }// end of phase two
-
-			
-			Map<String, Integer> attributesIndex = classifierOps
-					.getAttributesWithIndexes(inputTrain);
-			Map<String, BasicMethods> bestMethod4Attributes = new HashMap<String, BasicMethods>();
-			Instances outputTrain = inputTrain;
-//			System.out.println(attributesIndex);
-
-			// 3- best basic discretization methods for continuous attributes
-			// are determined one by one according to their ranks
-			for (int i = 0; i < sortedDependencies.size(); i++) {
-					// 3-1 test all basic methods for this attribute
-					Attribute currentAttr = sortedDependencies.get(i);
-//
-					BasicMethods bestMethodCurrent = findBestAccuracy(attributesIndex.get(currentAttr.name()),
-																		currentAttr, outputTrain);
-					
-					int[] attributesIndecis0 = new int[] { attributesIndex
-							.get(sortedDependencies.get(i).name()) };
-					DiscretizedInstance disretizedResult0 = applyDiscretization(bestMethodCurrent,
-							outputTrain, attributesIndecis0);
-					outputTrain = disretizedResult0.getOutputTrain();
-				// **********************************************************************
-					
-					bestMethod4Attributes.put(currentAttr.name(), bestMethodCurrent);
-					
-				// **********************************************************************
-					
-
-				// 3-2 test other attributes with best basic method
-				
-				for(int j = 0; j < i; j++){
-					Attribute beforeCurrentAttr = sortedDependencies.get(j);
-					int[] attributesIndecis = new int[] { attributesIndex
-							.get(sortedDependencies.get(i).name()) };
-					if(bestMethod4Attributes.get(beforeCurrentAttr.name()) != null){
-						DiscretizedInstance disretizedResult = applyDiscretization(bestMethod4Attributes.get(beforeCurrentAttr.name()),
-								outputTrain, attributesIndecis);
-						
-						outputTrain = disretizedResult.getOutputTrain();
-					}
-					else{
-						DiscretizedInstance disretizedResult = applyDiscretization(bestMethod,
-								outputTrain, attributesIndecis);
-						
-						outputTrain = disretizedResult.getOutputTrain();
-					}
-				}
-				for(int j = i+1; j < sortedDependencies.size(); j++){
-					Attribute afterCurrentAttr = sortedDependencies.get(j);
-					int[] attributesIndecis = new int[] { attributesIndex
-							.get(sortedDependencies.get(i).name()) };
-					if(bestMethod4Attributes.get(afterCurrentAttr.name()) != null){
-						DiscretizedInstance disretizedResult = applyDiscretization(bestMethod4Attributes.get(afterCurrentAttr),
-								outputTrain, attributesIndecis);
-						
-						outputTrain = disretizedResult.getOutputTrain();
-					}
-					else{
-						DiscretizedInstance disretizedResult = applyDiscretization(bestMethod,
-								outputTrain, attributesIndecis);
-						
-						outputTrain = disretizedResult.getOutputTrain();
-					}
-				}
-				
-			}
-			
-			NaiveBayes nB = new NaiveBayes();
-			nB.buildClassifier(outputTrain);
-			Evaluation eval = new Evaluation(outputTrain);
-			eval.crossValidateModel(nB, outputTrain, 5, new Random(1));
-			
-			System.out.println("Final result is going to be printed:\n------------------------------------------");
-			System.out.println(classifierOps.calculateAccuracy(eval.predictions()));
-			System.out.println("------------------------------------------");
-			
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} finally {
+		String[] fileNames = new String[]{"glass","haberman","ionosphere","iris","liver-disorders","hypothyroid","segment","vehicle"};
+		double[] firstResults = new double[8];
+		double[] finalResults = new double[8];
+		
+		for(int k = 0; k < fileNames.length; k++){
+			BufferedReader dataFile = null;
 			try {
-				dataFile.close();
-			} catch (IOException e) {
+				Instances inputTrain = null;
+				 
+				BasicMethods bestMethod = null;
+				// NaiveBayes nB = new NaiveBayes();
+	
+				BasicDiscretizers basicDiscretizers = new BasicDiscretizers();
+				ClassifierOperations classifierOps = new ClassifierOperations();
+	
+				dataFile = basicDiscretizers
+						.readDataFile("C:\\Program Files\\Weka-3-7\\data\\new\\" + fileNames[k] + ".arff");
+				inputTrain = basicDiscretizers.loadData(dataFile);
+	
+				// 1- rank continuous attributes by the nonparametric measure.
+				Map<Attribute, Double> dependencyLevel = classifierOps
+						.getDepencencyLevel(inputTrain);
+				List<Attribute> sortedDependencies = classifierOps
+						.sortDependencyLevels(dependencyLevel);
+	
+				// 2- for all continuous attributes,
+				// find the best one that achieves the largest prediction accuracy
+				// from the four basic discretization methods {
+	
+				// 2-1 Equal-width discretization
+				double accuracy1 = applyDiscretization(BasicMethods.EQUAL_WIDTH,
+						inputTrain, null).getAccuracy();
+	//			System.out.println(accuracy1);
+	
+				// 2-2 Equal-frequency discretization
+	//			System.out.println("=============================================");
+	
+				double accuracy2 = applyDiscretization(
+						BasicMethods.EQUAL_FREQUENCY, inputTrain, null).getAccuracy();
+	//			System.out.println(accuracy2);
+	
+				// 2-3 Entropy-based discretization
+	//			System.out.println("=============================================");
+				double accuracy3 = applyDiscretization(BasicMethods.ENTROPY_BASED,
+						inputTrain, null).getAccuracy();
+	//			System.out.println(accuracy3);
+	
+				// 2-4 find the highest accuracy among all accuracies
+				double max = Math.max(accuracy1, accuracy2);
+				max = Math.max(max, accuracy3);
+				if (max == accuracy1)
+					bestMethod = BasicMethods.EQUAL_WIDTH;
+				else if (max == accuracy2)
+					bestMethod = BasicMethods.EQUAL_FREQUENCY;
+				else if (max == accuracy3)
+					bestMethod = BasicMethods.ENTROPY_BASED;
+				
+				NaiveBayes nB1 = new NaiveBayes();
+				nB1.buildClassifier(inputTrain);
+				Evaluation eval2 = new Evaluation(inputTrain);
+				eval2.crossValidateModel(nB1, inputTrain, 5, new Random(1));
+//				System.out.println("first is: " + classifierOps.calculateAccuracy(eval2.predictions()));
+				firstResults[k] = classifierOps.calculateAccuracy(eval2.predictions());
+	
+				// }// end of phase two
+	
+				
+				Map<String, Integer> attributesIndex = classifierOps
+						.getAttributesWithIndexes(inputTrain);
+				Map<String, BasicMethods> bestMethod4Attributes = new HashMap<String, BasicMethods>();
+				Instances outputTrain = inputTrain;
+	//			System.out.println(attributesIndex);
+	
+				// 3- best basic discretization methods for continuous attributes
+				// are determined one by one according to their ranks
+				for (int i = 0; i < sortedDependencies.size(); i++) {
+						// 3-1 test all basic methods for this attribute
+						Attribute currentAttr = sortedDependencies.get(i);
+	//
+						BasicMethods bestMethodCurrent = findBestAccuracy(attributesIndex.get(currentAttr.name()),
+																			currentAttr, outputTrain);
+						
+						int[] attributesIndecis0 = new int[] { attributesIndex
+								.get(sortedDependencies.get(i).name()) };
+						DiscretizedInstance disretizedResult0 = applyDiscretization(bestMethodCurrent,
+								outputTrain, attributesIndecis0);
+						outputTrain = disretizedResult0.getOutputTrain();
+					// **********************************************************************
+						
+						bestMethod4Attributes.put(currentAttr.name(), bestMethodCurrent);
+						
+					// **********************************************************************
+						
+	
+					// 3-2 test other attributes with best basic method
+					
+					for(int j = 0; j < i; j++){
+						Attribute beforeCurrentAttr = sortedDependencies.get(j);
+						int[] attributesIndecis = new int[] { attributesIndex
+								.get(sortedDependencies.get(i).name()) };
+						if(bestMethod4Attributes.get(beforeCurrentAttr.name()) != null){
+							DiscretizedInstance disretizedResult = applyDiscretization(bestMethod4Attributes.get(beforeCurrentAttr.name()),
+									outputTrain, attributesIndecis);
+							
+							outputTrain = disretizedResult.getOutputTrain();
+						}
+						else{
+							DiscretizedInstance disretizedResult = applyDiscretization(bestMethod,
+									outputTrain, attributesIndecis);
+							
+							outputTrain = disretizedResult.getOutputTrain();
+						}
+					}
+					for(int j = i+1; j < sortedDependencies.size(); j++){
+						Attribute afterCurrentAttr = sortedDependencies.get(j);
+						int[] attributesIndecis = new int[] { attributesIndex
+								.get(sortedDependencies.get(i).name()) };
+						if(bestMethod4Attributes.get(afterCurrentAttr.name()) != null){
+							DiscretizedInstance disretizedResult = applyDiscretization(bestMethod4Attributes.get(afterCurrentAttr),
+									outputTrain, attributesIndecis);
+							
+							outputTrain = disretizedResult.getOutputTrain();
+						}
+						else{
+							DiscretizedInstance disretizedResult = applyDiscretization(bestMethod,
+									outputTrain, attributesIndecis);
+							
+							outputTrain = disretizedResult.getOutputTrain();
+						}
+					}
+					
+				}
+				
+				NaiveBayes nB = new NaiveBayes();
+				nB.buildClassifier(outputTrain);
+				Evaluation eval = new Evaluation(outputTrain);
+				eval.crossValidateModel(nB, outputTrain, 5, new Random(1));
+				
+				finalResults[k] = classifierOps.calculateAccuracy(eval.predictions());
+//				System.out.println("Final result is going to be printed:\n------------------------------------------");
+//				System.out.println(classifierOps.calculateAccuracy(eval.predictions()));
+//				System.out.println("------------------------------------------");
+				
+	
+			} catch (Exception e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} finally {
+				try {
+					dataFile.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
 		}
-
+		
+//		System.out.println("Dataset:\t\t\tSame:\t\t\tHybrid result");
+		System.out.printf("%15s  %15s   %15s %55s%n", "Dataset", "Same  ", "Hybrid result","");
+		System.out.println("--------------------------------------------------------------");
+//		System.out.printf("%1s  %-7s   %-7s   %-6s   %-6s%n", "n", "result1", "result2", "time1", "time2");
+		for(int i = 0; i < fileNames.length; i++){
+//			System.out.println(fileNames[i] + "\t\t" + firstResults[i] + "\t\t" + finalResults[i]);
+			System.out.printf("%15s  %15f   %15f %55s%n", fileNames[i], firstResults[i], finalResults[i],"");
+		}
 	}
 	
 	public static BasicMethods findBestAccuracy(int attributesIndex, Attribute currentAttr, Instances inputTrain){
